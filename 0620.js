ad338a8dbd9801139b2942d7e95e2bf893412d80
jiangqb <jiangqb@citycloud.com.cn>
2021-07-26 14:33:13 +0800

    feat(0620.js promise.js promise.md): 更正文件

--- 0620.js
+++ 0620.js
@@ -2,38 +2,38 @@
 // 存在一个request(option, callback)函数用来进行ajax请求
 // 请使用 Promise 实现一个retry(option, count)函数。
 function __request__(option) {
-    return new Promise((resolve, reject) => {
-        request(option, (err, data) => {
-            if (err) reject(err)
-            else resolve(dats)
-        })
+  return new Promise((resolve, reject) => {
+    request(option, (err, data) => {
+      if (err) reject(err)
+      else resolve(dats)
     })
+  })
 }
 
 function retry(option, count) {
-    let promise = Promise.resolve()
-    let isStoped = false
-    for (let i = 0; i < count; ++i) {
-        promise = promise
-            .then((data) => {
-                if (!isStoped) {
-                    return __request__(option)
-                }
-                return data
-            })
-            .then(data => {
-                if (!isStoped) {
-                    isStoped = true
-                    return data
-                }
-            })
-            .catch(err => {
-                if (i >= count) {
-                    return Promise.reject(err)
-                }
-            })
-    }
-    return promise
+  let promise = Promise.resolve()
+  let isStoped = false
+  for (let i = 0; i < count; ++i) {
+    promise = promise
+      .then((data) => {
+        if (!isStoped) {
+          return __request__(option)
+        }
+        return data
+      })
+      .then((data) => {
+        if (!isStoped) {
+          isStoped = true
+          return data
+        }
+      })
+      .catch((err) => {
+        if (i >= count) {
+          return Promise.reject(err)
+        }
+      })
+  }
+  return promise
 }
 
 // 题目二：
@@ -49,32 +49,32 @@ function retry(option, count) {
 //     request("/user", { id: i }).then(console.log); /* 同一时刻至多pool个请求发起 */
 // }
 function createRequest({ pool }) {
-    const queue = []
-    const cache = []
-    const handler = (url, option) => {
-        const promise = ajax(url, option)
-        queue.push(promise)
-        return promise.then(data => {
-            // 一旦成功则将该promise从数组中删除
-            const index = queue.indexOf(promise)
-            index > -1 && queue.splice(index, 1)
-            // 同时从cache中取出一个来进行执行程序
-            const { resolve } = cache.unshift()
-            resolve && resolve()
-            return data
-        })
+  const queue = []
+  const cache = []
+  const handler = (url, option) => {
+    const promise = ajax(url, option)
+    queue.push(promise)
+    return promise.then((data) => {
+      // 一旦成功则将该promise从数组中删除
+      const index = queue.indexOf(promise)
+      index > -1 && queue.splice(index, 1)
+      // 同时从cache中取出一个来进行执行程序
+      const { resolve } = cache.unshift()
+      resolve && resolve()
+      return data
+    })
+  }
+  return (url, option) => {
+    if (queue.length < pool) {
+      const promise = ajax(url, option)
+      queue.push(promise)
+      return handler(url, option)
+    } else {
+      return new Promise((resolve) => {
+        cache.push({ url, option, resolve })
+      }).then(() => {
+        return handler(url, option)
+      })
     }
-    return (url, option) => {
-        if (queue.length < pool) {
-            const promise = ajax(url, option)
-            queue.push(promise)
-            return handler(url, option)
-        } else {
-            return new Promise(resolve => {
-                cache.push({ url, option, resolve })
-            }).then(() => {
-                return handler(url, option)
-            })
-        }
-    }
+  }
 }
