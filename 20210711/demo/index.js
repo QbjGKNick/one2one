a60a268f8f99373916cc4ea84501f19d35bf0a05
jiangqb <jiangqb@citycloud.com.cn>
2021-09-13 07:58:28 +0800

    fix(lais): lais build success

--- 20210711/demo/index.js
+++ 20210711/demo/index.js
@@ -1,8 +1,11 @@
-import Vue from 'vue'
-import App from './App.vue'
-import 'normalize.css'
+// import Vue from 'vue'
+// import App from './App.vue'
+// import 'normalize.css'
 
-new Vue({
-  el: '#app',
-  render: (h) => h(App)
-})
+// new Vue({
+//   el: '#app',
+//   render: (h) => h(App)
+// })
+
+const App = reuquire('./App.vue')
+const _ = require("lodash")
